c8a680fb529ade511ab59596b2a4991b078afb74
DevJ2K <ajavontheopro@gmail.com>
2024-11-08 19:44:27 +0100

    💭 : Start to implement game_state...

--- backend/gomoku/gomoku_algorithm.py
+++ backend/gomoku/gomoku_algorithm.py
@@ -29,7 +29,10 @@ def game_state(gomoku: LittleGomoku) -> tuple[int, str]:
 	# - (?) No Free Three for opponent (?)
 	# - No Free Four for opponent
 	"""
-	pass
+	if gomoku.player_turn == gomoku.maximizing_player:
+		return random.randint(1, 5)
+	else:
+		return random.randint(-5, -1)
 
 def simulate_action(gomoku: LittleGomoku) -> LittleGomoku:
 	"""
@@ -50,15 +53,15 @@ def minimax(
 	# print(f"===== DEPTH : {DEPTH} =====")
 	# print("=====================")
 	if DEPTH == MAX_DEPTH:
-		# return 5, None
-		if gomoku.player_turn == gomoku.maximizing_player:
-			return random.randint(0, 5), None
-		else:
-			return random.randint(-5, 0), None
-		# return random.choice([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]), None
-		return 0, None
-	# if terminate_state(board) or DEPTH == MAX_DEPTH:
-	# 	return game_state(gomoku), None
+		return game_state(gomoku), None
+
+	if terminate_state(
+		gomoku.board,
+		gomoku.black_capture,
+		gomoku.white_capture,
+		gomoku.settings
+		) or DEPTH == MAX_DEPTH:
+		return game_state(gomoku), None
 
 	if gomoku.player_turn == gomoku.maximizing_player:
 		value = float('-inf')
@@ -68,13 +71,13 @@ def minimax(
 				new_gomoku = gomoku.simulate_action(action)
 			except:
 				print("Failed to simulate")
-				print(gomoku)
-				print(is_creating_db_free_three(gomoku.board, action[0], action[1], gomoku.player_turn))
-				print(gomoku.is_valid_placement(i=action[0], j=action[1], stone=gomoku.player_turn))
-				gomoku.board[action[0]][action[1]] = gomoku.player_turn
-				print(gomoku)
-				print(action)
-				exit(1)
+				# print(gomoku)
+				# print(is_creating_db_free_three(gomoku.board, action[0], action[1], gomoku.player_turn))
+				# print(gomoku.is_valid_placement(i=action[0], j=action[1], stone=gomoku.player_turn))
+				# gomoku.board[action[0]][action[1]] = gomoku.player_turn
+				# print(gomoku)
+				# print(action)
+				# exit(1)
 				continue
 			state, r_action = minimax(new_gomoku, alpha, beta, DEPTH + 1, MAX_DEPTH=MAX_DEPTH)
 
@@ -83,7 +86,7 @@ def minimax(
 				best_action = action
 
 			alpha = max(alpha, state)
-			if beta <= alpha or state == 1:
+			if beta <= alpha or state == 6:
 				break
 		return value, best_action
 
@@ -101,7 +104,7 @@ def minimax(
 				value = state
 				best_action = action
 			beta = min(beta, state)
-			if beta <= alpha or state == -1:
+			if beta <= alpha or state == -6:
 				break
 		return value, best_action
 	else:
@@ -119,7 +122,7 @@ if __name__ == "__main__":
 	gomoku.place_stone("J8", "B")
 	gomoku.place_stone("H9", "W")
 	gomoku.place_stone("J10", "B")
-	gomoku.place_stone("R18", "W")
+	# gomoku.place_stone("R18", "W")
 	gomoku.switch_player_turn()
 
 	littleGomoku = LittleGomoku(
@@ -144,7 +147,7 @@ if __name__ == "__main__":
 
 	measureTime = MeasureTime(start=True)
 	# print(littleGomoku.get_actions())
-	print(minimax(littleGomoku, MAX_DEPTH=3))
+	print(minimax(littleGomoku, MAX_DEPTH=2))
 	measureTime.stop()
 
 	from gomoku_rules import is_creating_db_free_three, is_free_three
