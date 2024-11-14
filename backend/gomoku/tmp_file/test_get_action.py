50a2067d996eead15caf5f2620f2db367b7671b6
theo <ajavontheopro@gmail.com>
2024-11-14 22:45:01 +0100

    ❌ : Filter using game state filter is not optimized.

--- backend/gomoku/tmp_file/test_get_action.py
+++ backend/gomoku/tmp_file/test_get_action.py
@@ -4,12 +4,21 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from Gomoku import Gomoku
+from gomoku_algorithm import minimax
+from little_gomoku_utils import convert_to_little_gomoku
+from MeasureTime import MeasureTime
 
 if __name__ == "__main__":
 	gomoku = Gomoku()
-	try:
-		gomoku.read_a_game(17, 0, live_visualisation=False, live_speed=0.5)
-	except Exception as e:
-		print(e)
-		pass
+	gomoku.read_a_game(9, -11, live_visualisation=False, live_speed=0.5)
 	print(gomoku)
+	littleGomoku = convert_to_little_gomoku(gomoku)
+	mt = MeasureTime(start=True)
+	score, move = minimax(gomoku=littleGomoku, MAX_DEPTH=2)
+	mt.stop()
+	# DEPTH 2 WITHOUT : 113ms
+	# DEPTH 2 WITH : 776ms
+	# DEPTH 4 : 17064ms
+	print(score)
+	print(move)
+	# print(gomoku.player_turn)
