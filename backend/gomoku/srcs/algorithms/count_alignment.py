fbb5d9a35afb889446b0a3dd6fb4fa5604259fc0
DevJ2K <ajavontheopro@gmail.com>
2024-11-26 20:19:26 +0100

    ✅ : Adjust regex patterns and add in alignment streaks with some tests !

--- backend/gomoku/srcs/algorithms/count_alignment.py
+++ backend/gomoku/srcs/algorithms/count_alignment.py
@@ -5,28 +5,69 @@ def check_alignment(row: str, stone: str, stone_count: int) -> bool:
 	return row.count(stone) == stone_count and row.count(" ") == len(row) - stone_count
 	pass
 
+# def type_of_alignment(row: str, stone: str):
+# 	opponent_stone = switch_opponent(stone)
+# 	opponent_count = row.count(opponent_stone)
+# 	stone_count = row.count(stone)
+
+# 	if opponent_count > 1:
+# 		return 'invalid', 0
+# 	if opponent_count == 1:
+# 		if row[0] != opponent_stone and row[-1] != opponent_stone:
+# 			return 'invalid', 0
+# 	if row[0] == " " and row[-1] == " ":
+# 		row_split = row.split(" ")
+# 		max_align = 0
+# 		for item in row_split:
+# 			if len(item) > max_align:
+# 				max_align = len(item)
+# 			if len(item) == stone_count:
+# 				if stone_count >= 5:
+# 					return 'align', 5
+# 				return 'free', stone_count
+# 		if max_align == 2 and stone_count == 3:
+# 			return 'free', 3
+# 		return 'free', max_align
+# 	else:
+# 		return 'align', stone_count
+
 def type_of_alignment(row: str, stone: str):
+	print(f"'{row}'")
 	opponent_stone = switch_opponent(stone)
-	opponent_count = row.count(opponent_stone)
-	if opponent_count > 1:
-		return 'invalid', 0
-	if opponent_count == 1:
-		if row[0] != opponent_stone and row[-1] != opponent_stone:
-			return 'invalid', 0
-	if row[0] == " " and row[-1] == " ":
-		row_split = row.split(" ")
-		nb_stone = row.count(stone)
-		max_align = 0
-		for item in row_split:
-			if len(item) > max_align:
-				max_align = len(item)
-			if len(item) == nb_stone:
-				return 'free', nb_stone
-		if max_align == 2 and nb_stone == 3:
-			return 'free', 3
-		return 'free', max_align
-	else:
-		return 'align', row.count(stone)
+	stone_count = row.count(stone)
+
+	# if opponent_count > 1:
+	# 	return 'invalid', 0
+	# if opponent_count >= 1:
+	# 	if row[0] != opponent_stone and row[-1] != opponent_stone:
+	# 		return 'invalid', 0
+
+
+	row_split = row.split(" ")
+	max_align = 0
+	for item in row_split:
+		if item.count(stone) > max_align:
+			max_align = item.count(stone)
+		if item.count(stone) == stone_count:
+			if stone_count >= 5:
+				return 'align', 5
+
+
+	if stone_count == 3:
+		if row[0] == " " and row[-1] == " ":
+			return "free", stone_count
+		return "align", stone_count
+
+	if max_align == 4:
+		i_start = row.find(4 * stone)
+		i_end = i_start + 3
+
+		i_start = i_start - 1 if i_start - 1 >= 0 else i_start
+		i_end = i_end + 1 if i_end + 1 < len(row) - 1 else len(row) - 1
+
+		if row[i_start] == " " and row[i_end] == " ":
+			return "free", max_align
+	return 'align', 4
 
 def is_three_aligned(board: list[list[str]], i, j, stone):
 	count = 0
